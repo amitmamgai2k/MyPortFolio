fba5b1ac19305affda3ddfa963b087f79407ebb5
amitmamgai2k <123645217+amitmamgai2k@users.noreply.github.com>
2025-04-20 15:06:41 +0530

    Update file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
-import AllProjects from './components/All Projects'
+import AllProjects from './components/AllProjects'
 import PageNotFound from './pages/404'
 
 function App() {
